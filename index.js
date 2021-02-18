3e19731de1b69cd08c264e5664bfa8fc1a268bc2
Chris Zelenak <chris@zelenak.me>
2021-02-18 11:17:51 -0500

    remove flow

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-// @flow
 
 import { NativeModules } from 'react-native';
 
 const { RNReverseGeocode } = NativeModules;
-
-type Location = {|
-  +latitude: number,
-  +longitude: number,
-|};
-
-type Address = {|
-  name: string,
-  address: string,
-  location: Location,
-|};
-
-type Result = $ReadOnlyArray<Address>;
-
-type Region = {|
-  ...Location,
-  +latitudeDelta: number,
-  +longitudeDelta: number,
-|};
-
-type Callback = (err: string, res: Result) => void;
 
 const debounce = (fn, time, ...args) => {
   let timeout;
